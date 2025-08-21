45503022bbe1bbe4a51e7689a076260f39326ad8
Amicis Innovations <info@amicisinnovations.co.za>
2025-08-21 10:32:43 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const lm1 = document.getElementById("learn1");
+  myButton.addEventListener("click", () => {
+    alert("click click");
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const lm2 = document.getElementById("learn2");
+  myButton.addEventListener("click", () => {
+    alert("click click");
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const navLogo = document.getElementById("nav-logo");
+  navLogo.addEventListener("click", () => {
+    window.location.href = "index.html";
+  });
+});
