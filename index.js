20813c5101dc6330a336aa91bca79827625aba19
JLDHarold <jldharold@gmail.com>
2025-10-15 11:15:05 +0200

    Some changes

--- index.js
+++ index.js
@@ -24,3 +24,12 @@ document.addEventListener("DOMContentLoaded", () => {
     window.location.href = "index.html";
   });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.getElementById('mobile-menu');
+  const navMenu = document.querySelector('.nav-menu');
+
+  menuToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('show');
+  });
+});
